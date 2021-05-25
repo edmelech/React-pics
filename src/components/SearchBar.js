05c83a3126bfee5a4660ff5f40b33078f03111f1
Ed Melech <mr.melech@gmail.com>
2021-05-25 15:48:59 +0100

    create App and Search Bar components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render () {
+    return (
+      <div>
+        <form>
+          <input typr="text" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
